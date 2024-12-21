bee348d4a66107fd6677b91eb1bc5955e5c8612b
Nikhil Dhanda <nikhildhanda84@gmail.com>
2024-12-22 02:04:38 +0530

    changed title

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -13,8 +13,8 @@ import Blog from "@/components/Blog";
 import Testimonial from "@/components/Testimonial";
 
 export const metadata: Metadata = {
-  title: "Next.js Starter Template for SaaS Startups - Solid SaaS Boilerplate",
-  description: "This is Home for Solid Pro",
+  title: "Upliftio",
+  description: "Growing Together",
   // other metadata
 };
 
